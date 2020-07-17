de17aebb72357a6ee4a205792f3960b15e3a4d99
Sarah Kelly <s.kelly@imperial.ac.uk>
2020-07-17 11:32:09 +0100

    added mexico config files

--- R/writting_config_files.R
+++ R/writting_config_files.R
@@ -234,3 +234,36 @@ province_level <- canada_cumulative_case_count_by_new_hybrid_regional_health_bou
   tibble::add_column(standard = NA) %>%
   mutate(standard = ifelse(is.na(standard), provided, standard))
 write.csv(province_level, file = "/Users/sakelly/maps/canada_cumulative_case/province_level.csv",  row.names = FALSE)
+
+### mexico ###
+
+column_mapping <- cfg_write(provided = c("report_date", "state", "sex", "age", "date_of_start_of_symptoms", "status", "origin", "date_of_arrival_in_mexico", "report_url"), 
+                            standard = c("date", "state", "sex", "age", "date_of_start_of_symptoms", "status", "origin", "date_of_arrival_in_mexico", "report_url"),
+                            table = column_mapping, 
+                            path = "/Users/sakelly/maps/mexico/column_mapping.csv") 
+
+state_level <- covid19_mexico_daily_cases %>%
+  dplyr::distinct(state) %>%
+  rename(provided = state) %>%
+  tibble::add_column(standard = NA) %>%
+  mutate(standard = ifelse(is.na(standard), provided, standard))
+write.csv(state_level, file = "/Users/sakelly/maps/mexico/state_level.csv",  row.names = FALSE)
+
+sex_levels <- covid19_mexico_daily_cases %>%
+  dplyr::distinct(sex) %>%
+  rename(provided = sex) %>%
+  tibble::add_column(standard = NA) %>%
+  mutate(standard = c("male", "female"))
+write.csv(sex_levels, file = "/Users/sakelly/maps/mexico/sex_levels.csv", row.names = FALSE)
+
+status_levels <- covid19_mexico_daily_cases %>%
+  dplyr::distinct(status) %>%
+  rename(provided = status) %>%
+  tibble::add_column(standard = NA)
+write.csv(status_levels, file = "/Users/sakelly/maps/mexico/status_levels.csv", row.names = FALSE)
+
+origin_levels <- covid19_mexico_daily_cases %>%
+  dplyr::distinct(origin) %>%
+  rename(provided = origin) %>%
+  tibble::add_column(standard = NA)
+write.csv(origin_levels, file = "/Users/sakelly/maps/mexico/origin_levels.csv", row.names = FALSE)
