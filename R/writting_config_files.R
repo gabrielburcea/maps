5fa4bd5aa9ad7a85b71a1f9b88937ff87c7f7889
Sarah Kelly <s.kelly@imperial.ac.uk>
2020-07-20 13:35:01 +0100

    Added config files for belgium_covid19_cases_muni - BELGIUM

--- R/writting_config_files.R
+++ R/writting_config_files.R
@@ -367,3 +367,58 @@ region_level <- COVID19BE_HOSP %>%
   tibble::add_column(standard = NA) %>%
   mutate(standard = ifelse(is.na(standard), provided, standard))
 write.csv(region_level, file = "/Users/sakelly/maps/belgium_covid19_cases_hosp/region_level.csv",  row.names = FALSE)
+
+### belgium_covid19_cases_muni - BELGIUM ###
+
+column_mapping <- cfg_write(provided = c("DATE", "NIS5", "TX_DESCR_NL", "TX_DESCR_FR", "TX_ADM_DSTR_DECSR_NL", "TX_ADM_DSTR_DESCR_FR", "TX_PROV_DESCR_NL", "TX_PROV_DESCR_FR", "TX_RGN_DESCR_NL", "TX_RGN_DESCR_FR", "CASES"), 
+                            standard = c("date", "nis5", "tx_descr_nl", "tx_descr_fr", "tx_adm_dstr_descr", "tx_adm_dstr_descr_fr", "tx_prov_descr_nl", "tx_prov_descr_fr", "tx_rgn_descr_nl", "tx_rgn_descr_fr", "cases"),
+                            table = column_mapping, 
+                            path = "/Users/sakelly/maps/belgium_covid19_cases_muni/column_mapping.csv")
+
+tx_descr_nl_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_DESCR_NL) %>%
+  rename(provided = TX_DESCR_NL) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_descr_nl_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_descr_nl_level.csv",  row.names = FALSE)
+
+tx_descr_fr_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_DESCR_FR) %>%
+  rename(provided = TX_DESCR_FR) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_descr_fr_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_descr_fr_level.csv",  row.names = FALSE)
+
+tx_adm_dstr_descr_nl_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_ADM_DSTR_DESCR_NL) %>%
+  rename(provided = TX_ADM_DSTR_DESCR_NL) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_adm_dstr_descr_nl_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_adm_dstr_descr_nl_level.csv",  row.names = FALSE)
+
+tx_adm_dstr_descr_fr_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_ADM_DSTR_DESCR_FR) %>%
+  rename(provided = TX_ADM_DSTR_DESCR_FR) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_adm_dstr_descr_fr_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_adm_dstr_descr_fr_level.csv",  row.names = FALSE)
+
+tx_prov_descr_nl_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_PROV_DESCR_NL) %>%
+  rename(provided = TX_PROV_DESCR_NL) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_prov_descr_nl_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_prov_descr_nl_level.csv",  row.names = FALSE)
+
+tx_prov_descr_fr_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_PROV_DESCR_FR) %>%
+  rename(provided = TX_PROV_DESCR_FR) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_prov_descr_fr_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_prov_descr_fr_level.csv",  row.names = FALSE)
+
+tx_rgn_descr_nl_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_RGN_DESCR_NL) %>%
+  rename(provided = TX_RGN_DESCR_NL) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_rgn_descr_nl_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_rgn_descr_nl_level.csv",  row.names = FALSE)
+
+tx_rgn_descr_fr_level <- COVID19BE_CASES_MUNI %>%
+  dplyr::distinct(TX_RGN_DESCR_FR) %>%
+  rename(provided = TX_RGN_DESCR_FR) %>%
+  tibble::add_column(standard = NA)
+write.csv(tx_rgn_descr_fr_level, file = "/Users/sakelly/maps/belgium_covid19_cases_muni/tx_rgn_descr_fr_level.csv",  row.names = FALSE)
