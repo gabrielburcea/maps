840f81063c0b9d8e0468f579acb6298cd2ebe48e
Sarah Kelly <s.kelly@imperial.ac.uk>
2020-07-21 15:52:23 +0100

    Added config file for worldwide_owid_covid_data - query does it say date is categorical in here?

--- R/writting_config_files.R
+++ R/writting_config_files.R
@@ -561,3 +561,29 @@ fips_level <- us_counties %>%
   rename(provided = fips) %>%
   tibble::add_column(standard = NA)
 write.csv(fips_level, file = "/Users/sakelly/maps/us_counties_statistics_states/fips_level.csv",  row.names = FALSE)
+
+### worldwide_owid-covid_data ###
+
+column_mapping <- cfg_write(provided = c("iso_code", "continent", "location", "date", "total_cases", "new_cases", "total_deaths", "new_deaths", "total_cases_per_million", "new_cases_per_million", "total_deaths_per_million", "new_deaths_per_million", "total_tests", "new_tests", "total_tests_per_thousand", "new_tests_per_thousand", "new_tests_per_thousand", "new_tests_smoothed", "new_tests_smoothed_per_thousand", "tests_unit", "stringency_index", "population", "population_density", "median_age", "aged_65_older", "aged_70_older", "gdp_per_capita", "extreme_poverty", "cvd_death_rate", "diabetes_prevalence", "female_smokers", "male_smokers", "handwashing_facilities", "hospital_beds_per_thousand"), 
+                            standard = c("iso_code", "continent", "location", "date", "total_cases", "new_cases", "total_deaths", "new_deaths", "total_cases_per_million", "new_cases_per_million", "total_deaths_per_million", "new_deaths_per_million", "total_tests", "new_tests", "total_tests_per_thousand", "new_tests_per_thousand", "new_tests_per_thousand", "new_tests_smoothed", "new_tests_smoothed_per_thousand", "tests_unit", "stringency_index", "population", "population_density", "median_age", "aged_65_older", "aged_70_older", "gdp_per_capita", "extreme_poverty", "cvd_death_rate", "diabetes_prevalence", "female_smokers", "male_smokers", "handwashing_facilities", "hospital_beds_per_thousand"),
+                            table = column_mapping, 
+                            path = "/Users/sakelly/maps/worldwide_owid-covid_data/column_mapping.csv")
+
+iso_code_level <- owid_covid_data %>%
+  dplyr::distinct(iso_code) %>%
+  rename(provided = iso_code) %>%
+  tibble::add_column(standard = NA)
+write.csv(iso_code_level, file = "/Users/sakelly/maps/worldwide_owid-covid_data/iso_code_level.csv",  row.names = FALSE)
+
+continent_level <- owid_covid_data %>%
+  dplyr::distinct(continent) %>%
+  rename(provided = continent) %>%
+  tibble::add_column(standard = NA) %>%
+  mutate(standard = ifelse(is.na(standard), provided, standard))
+write.csv(continent_level, file = "/Users/sakelly/maps/worldwide_owid-covid_data/continent_level.csv",  row.names = FALSE)
+
+location_level <- owid_covid_data %>%
+  dplyr::distinct(location) %>%
+  rename(provided = location) %>%
+  tibble::add_column(standard = NA)
+write.csv(location_level, file = "/Users/sakelly/maps/worldwide_owid-covid_data/location_level.csv",  row.names = FALSE)
