449a2efc39360889a673d7e554270b4128ca0024
healthpredict <gburcea@ic.ac.uk>
2020-09-28 15:52:24 +0100

    added a new script for constant values configuration files

---
+++ R/writting_constant_values_tables.R
@@ -0,0 +1,13 @@
+# mexico 
+
+
+constant_values <- const_val_fct(column_name = c("country" , "report_date", "state", "sex" , "age", "date_of_start_case_type", "status", "origin", "date_of_arrival_in_mexico", "report_url"), 
+                                 term_id =  c('ENVO:00000009', 'EUPATH:0000151', 'PATO:0000047', 'OBI:0001167', NA, NA, NA, NA, NA, NA),
+                                 constant_values =  c("Mexico",NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                                 values_id = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                                 units = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                                 units_id = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                                 notes = c(NA, "talk", NA, NA, NA, NA, NA, NA, NA, NA), 
+                                 table = constant_values,
+                                 path = "/Users/gabrielburcea/rprojects/maps/mexico/constant_values.csv"
+                                 )
