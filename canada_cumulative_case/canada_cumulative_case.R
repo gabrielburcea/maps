38bdd368fe39b85ffccf856e970ad1abde6deb0b
healthpredict <gburcea@ic.ac.uk>
2020-11-11 11:07:38 +0000

    pushing the work done by Sarah yet it appears I have to push them back to github. No clue why. but hopefylly you get your files back.

---
+++ canada_cumulative_case/canada_cumulative_case.R
@@ -0,0 +1,20 @@
+column_mapping <- cfg_write(provided = c("objectid", "hr_uid", "province", "engname", "frename", "casecount", "deaths", "recovered", "tests", "totalpop2019", "pop0to4_2019", "pop5to9_2019", "pop10to14_2019", "pop15to19_2019", "pop20to24_2019", "pop25to29_2019", "pop30to34_2019", "pop35to39_2019", "pop40to44_2019", "pop45to49_2019", "pop50to54_2019", "pop55to59_2019", "pop60to64_2019", "pop65to69_2019", "pop70to74_2019", "pop75to79_2019", "pop80to84_2019", "pop85older", "averageage_2019", "medianage_2019", "shape_area", "shape_length", "last_updated", "sourceurl", "globalid", "retreived_at"), 
+                            standard = c("objectid", "hr_uid", "geographic_location", "engname", "frename", "casecount", "deaths", "recovered", "tests", "totalpop2019", "pop0to4_2019", "pop5to9_2019", "pop10to14_2019", "pop15to19_2019", "pop20to24_2019", "pop25to29_2019", "pop30to34_2019", "pop35to39_2019", "pop40to44_2019", "pop45to49_2019", "pop50to54_2019", "pop55to59_2019", "pop60to64_2019", "pop65to69_2019", "pop70to74_2019", "pop75to79_2019", "pop80to84_2019", "pop85older", "averageage_2019", "medianage_2019", "shape_area", "shape_length", "last_updated", "sourceurl", "globalid", "retreived_at"),
+                            term_id = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                            units = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                            units_id = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                            value_mapping_file = c(NA, NA, "province_levels.csv", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                            notes = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                            table = column_mapping,
+                            path = "/Users/sakelly/maps/canada_cumulative_case/column_mapping.csv")
+
+
+constant_values <- const_val_fct(column_name = c("country", "case_type", "geographic_resolution"), 
+                                 term_id =  c('ENVO:00000009', NA, "ENVO:00000004"),
+                                 constant_values =  c("Canada", NA, "admin level 4"),
+                                 values_id = c(NA, NA, "ENVO:00000008"), 
+                                 units = c(NA, NA, NA), 
+                                 units_id = c(NA, NA, NA), 
+                                 notes = c(NA, NA, NA), 
+                                 table = constant_values,
+                                 path = "/Users/sakelly/maps/canada_cumulative_case/constant_values.csv")
