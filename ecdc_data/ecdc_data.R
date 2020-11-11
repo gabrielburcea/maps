0a0fe98fc74e4ae75d9d27256d5f5b18e893b2c2
healthpredict <gburcea@ic.ac.uk>
2020-11-11 11:15:17 +0000

    ecdc_data with the config files pushed

---
+++ ecdc_data/ecdc_data.R
@@ -0,0 +1,20 @@
+column_mapping <- cfg_write(provided = c("dateRep", "day", "month", "year", "cases", "deaths", "countriesAndTerritories", "geoId", "countryterritoryCode", "popData2018", "continentExp"),
+                            standard = c("date", "day", "month", "year", "cases", "deaths", "countriesAndTerritories", "geoId", "countryterritoryCode", "popData2018", "continentExp"),
+                            term_id = c(NA, NA, NA, NA,NA, NA, "GAZ:00000448", NA, NA, NA, NA),
+                            units = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                            units_id = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                            value_mapping_file = c(NA, NA, NA, NA, NA, NA, "countriesAndTerritories_levels.csv", "geoId_levels.csv", "countryterritoryCode_levels.csv", NA, "continentExp_levels.csv"),
+                            notes = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA), 
+                            table = column_mapping,
+                            path = "/Users/sakelly/maps/ecdc_data/column_mapping.csv")
+
+
+constant_values <- const_val_fct(column_name = c("case_type", "geographic_resolution"), 
+                                 term_id =  c(NA, "ENVO:00000004"),
+                                 constant_values =  c("cases", "admin level 0"),
+                                 values_id = c(NA, "ENVO???"), 
+                                 units = c(NA, NA), 
+                                 units_id = c(NA, NA), 
+                                 notes = c(NA, NA), 
+                                 table = constant_values,
+                                 path = "/Users/sakelly/maps/ecdc_data/constant_values.csv")
